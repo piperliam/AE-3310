4ae9428ebf2326f158730287f3421f16f32d0323
Piperliam <68978670+piperliam@users.noreply.github.com>
2022-09-02 18:56:34 -0400

    Add files via upload

---
+++ problem5.m
@@ -0,0 +1,37 @@
+%Liam Piper 
+%Assignment 1, Problem 5
+
+%import given data
+A = importdata('student_score_data.mat');
+n = 62;
+
+%Finding mean
+mu = sum(A,'all')/n;
+
+%Variance
+sigsq = var(A);
+
+%Standard Devivation
+sig = sqrt(sigsq);
+
+%Establish parameters
+plus = mu + sig;
+minus = mu - sig;
+minustwo = mu - 2*sig;
+AA = 0;
+BB = 0;
+
+for i=1:length(A)
+    if (A>70)
+        AA=AA+1
+        BB=BB
+    else if (plus > A >= minus)
+            BB = BB+1
+            AA=AA
+    else 
+        AA=AA
+        BB=BB
+    end
+
+    end
+end
